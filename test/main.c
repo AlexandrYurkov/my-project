3913bdcf821c75bf934d58b6311c12310c786f58
AlexandrYurkov <72255116+AlexandrYurkov@users.noreply.github.com>
2021-06-05 23:31:46 +0300

    Delete test directory
    second chance

--- test/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "ft_printf.h"
-
-int main()
-{
- //printf ( " { % d } \ n " , printf ( " \ 033 [1; 32mTest 18 => \ 033 [0m | % 05. * d | " , - 15 , 42 ));
-	
-ft_printf ("ssss");
-}
